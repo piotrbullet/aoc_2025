1aa89a8c1e1c4be6fb33f4b026a1b86e82ab0e5f
piotrbullet <piotrbullet69@gmail.com>
2025-12-11 22:36:11 +0100

    day 4 complete

--- day_4.py
+++ day_4.py
@@ -32,18 +32,36 @@ def part_1(path: str) -> int:
     return sum([x<4 for x in flat_score_array])
 
 def remove_rolls(grid: np.array, removed_rolls: int) -> int:
-    # neighbor_score_matrix = g
-    pass
+    curr_removed = 0
+    score_grid = np.array([[0 if x else 9 for x in y] for y in grid])
+    for iy, y in enumerate(grid):
+        for ix, x in enumerate(y):
+            if not x:
+                continue
+            for dx, dy in ADJACENTS:
+                nx, ny = ix+dx, iy+dy
+                if 0 <= nx < grid.shape[1] and 0 <= ny < grid.shape[0]:
+                    score_grid[ny][nx] += x
+    rm_grid = score_grid < 4
+    curr_removed = np.sum(rm_grid)
+
+    if curr_removed:
+        removed_rolls += curr_removed
+        grid[score_grid < 4] = 0
+        return remove_rolls(grid, removed_rolls)
+    else:
+        return removed_rolls
 
 def part_2(path: str) -> int:
     text = read_inputs(path).replace(".", "0").replace("@", "1")
     grid = [[int(val) for val in line] for line in text.splitlines()]
     grid = np.array(grid)
-    print(grid)
+    score = remove_rolls(grid, 0)
+    return score
 
 def main() -> None:
     print(f"Part 1 score: {part_1(INPUT_PATH)}")
-    print(f"Part 2 score: {part_2(TEST_INPUT_PATH)}")
+    print(f"Part 2 score: {part_2(INPUT_PATH)}")
 
 if __name__ == "__main__":
     main()
